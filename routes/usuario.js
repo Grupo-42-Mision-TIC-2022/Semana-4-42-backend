b94f63862a64d5e30eb761bedf293422cacefa6e
Juan Carlos Moreno <jcmhsoftware@gmail.com>
2020-12-18 01:37:42 -0500

    Primera versión del backend

---
+++ routes/usuario.js
@@ -0,0 +1,31 @@
+/* import routerx from 'express-promise-router';
+import usuarioController from '../controllers/UsuarioController';
+import auth from '../middlewares/auth'; */
+const routerx = require('express-promise-router');
+const usuarioController = require('../controllers/UsuarioController');
+//const auth = require('../middlewares/auth');
+
+const router = routerx();
+
+/*
+router.post('/add', auth.verifyUsuario, usuarioController.add);
+router.get('/query', auth.verifyUsuario, usuarioController.query);
+router.get('/list', auth.verifyUsuario, usuarioController.list);
+router.put('/update', auth.verifyUsuario, usuarioController.update);
+router.delete('/remove', auth.verifyUsuario, usuarioController.remove);
+router.put('/activate', auth.verifyUsuario, usuarioController.activate);
+router.put('/deactivate', auth.verifyUsuario, usuarioController.deactivate);
+router.post('/login', usuarioController.login);
+*/
+
+router.post('/add', usuarioController.add);
+router.get('/query', usuarioController.query);
+router.get('/list', usuarioController.list);
+router.put('/update', usuarioController.update);
+router.delete('/remove', usuarioController.remove);
+router.put('/activate', usuarioController.activate);
+router.put('/deactivate', usuarioController.deactivate);
+router.post('/login', usuarioController.login);
+
+
+module.exports = router;
